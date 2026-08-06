7469ae5e35fe8313bf875c20b28acfa81f8bc8e5
ZhangYuSha <isabellaaja21@gmail.com>
2026-08-07 02:32:33 +0800

    Modify HomeScreen Implementation (mostly regarding spacing name

--- src/pages/HomeScreen/HomeScreen.tsx
+++ src/pages/HomeScreen/HomeScreen.tsx
@@ -1,28 +1,27 @@
 import './HomeScreen.css'
-import Button from '../../component/Button.tsx'
-import { useNavigate } from 'react-router-dom';
+import Button from '../../component/Button'
+import { useNavigate } from 'react-router-dom'
 
 function HomeScreen() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleNavigation = (): void => {
-    navigate('/DataInput'); 
-  };
+    navigate('/DataInput')
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="Savings Goal Allocator">
-        </div>
-        <div>
-          <h1>Savings Goal Allocator</h1>
-          <p>
-            Plan your savings for the better future
-          </p>
-          <Button text="Create Goal" onClick={handleNavigation} />
-        </div>
-      </section>
-    </>
+    <section id="center">
+      <div className="SavingsGoalAllocator">
+      </div>
+
+      <div>
+        <h1>Savings Goal Allocator</h1>
+        <p>
+          Plan your savings for the better future
+        </p>
+        <Button text="Create Goal" onClick={handleNavigation} />
+      </div>
+    </section>
   )
 }
 
