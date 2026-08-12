55570d84da7912f2c67b9143b96d1786f3e89f58
ZhangYuSha <isabellaaja21@gmail.com>
2026-08-13 04:32:14 +0800

    Modify button click

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,28 +1,27 @@
-import "./Button.css";
+import './Button.css'
 
-interface ButtonProps {
-  text: string;
-  onClick?: () => void;
-  disabled?: boolean;
-  variant?: "primary" | "danger" | "secondary";
+type ButtonProps = {
+  text: string
+  onClick?: () => void
+  disabled?: boolean
 }
 
 function Button({
   text,
   onClick,
   disabled = false,
-  variant = "primary",
 }: ButtonProps) {
+
   return (
     <button
       type="button"
-      className={`button ${variant}`}
+      className="button"
       onClick={onClick}
       disabled={disabled}
     >
       {text}
     </button>
-  );
+  )
 }
 
-export default Button;
+export default Button
