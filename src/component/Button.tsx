ea3e3004894e444b6290117dddd9d5b302d6d6d0
ZhangYuSha <isabellaaja21@gmail.com>
2026-08-13 01:48:33 +0800

    Modify Button related classes

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,16 +1,24 @@
-import './Button.css'
+import "./Button.css";
 
-type ButtonProps = {
+interface ButtonProps {
   text: string;
   onClick?: () => void;
-};
+  disabled?: boolean;
+  variant?: "primary" | "danger" | "secondary";
+}
 
-function Button({ text, onClick }: ButtonProps) {
+function Button({
+  text,
+  onClick,
+  disabled = false,
+  variant = "primary",
+}: ButtonProps) {
   return (
     <button
       type="button"
-      className="button"
+      className={`button ${variant}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
